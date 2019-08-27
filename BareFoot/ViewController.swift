86d05ce7905ace7e075d52723bd7b0a1df0c0b5f
sankhura <sankhura@adobe.com>
2019-08-27 13:41:51 +0530

    Initial Commit

---
+++ BareFoot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BareFoot
+//
+//  Created by Sanya Khurana on 27/08/19.
+//  Copyright © 2019 Sanya Khurana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
