95c34c583a6b14f95b996bdad9e56c641ea56fc0
sankhura <sankhura@adobe.com>
2019-09-25 15:21:39 +0530

    taking photo throughout the app

--- BareFoot/Activity.swift
+++ BareFoot/Activity.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class Activity: UIViewController {
+class Activity: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var imagePicker: UIImagePickerController!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func takePhoto(_ sender: Any) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
     }
     
     override func didReceiveMemoryWarning() {
